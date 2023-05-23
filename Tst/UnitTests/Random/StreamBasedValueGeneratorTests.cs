1dab6dbe01fbe6d4903bc2e7e4f464a73d145a39
Ao Li <aolm@amazon.com>
2023-05-23 14:47:52 -0700

    Implement StreamBasedValueGenerator to support JQF style mutation and serialization.

---
+++ Tst/UnitTests/Random/StreamBasedValueGeneratorTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PChecker.Random;
+
+namespace UnitTests.Random
+{
+
+    [TestFixture]
+    public class StreamBasedValueGeneratorTests
+    {
+
+        internal class ControlledRandom : System.Random
+        {
+            private int i = 0;
+            public override int Next()
+            {
+                return i++;
+            }
+
+            public override void NextBytes(byte[] buffer)
+            {
+                var bytes = BitConverter.GetBytes(i++);
+                bytes.CopyTo(buffer, 0);
+            }
+        }
+
+        [Test]
+        public void TestGeneratorGenerateExpectedValue()
+        {
+            var random = new ControlledRandom();
+            var generator = new StreamBasedValueGenerator(random, null);
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(generator.Next(), i);
+            }
+        }
+
+        [Test]
+        public void TestGeneratorCopy()
+        {
+            var generator = new StreamBasedValueGenerator();
+            List<int> generated = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                generated.Add(generator.Next());
+            }
+            var newGenerator = new StreamBasedValueGenerator(generator);
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(generated[i], newGenerator.Next());
+            }
+        }
+    }
+}
