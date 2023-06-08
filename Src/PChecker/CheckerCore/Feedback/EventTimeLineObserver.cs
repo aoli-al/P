7bd30849c42a5bb751e4d925f9eb2b935649264f
Ao Li <aolm@amazon.com>
2023-06-08 14:04:54 -0700

    update.

--- Src/PChecker/CheckerCore/Feedback/EventTimeLineObserver.cs
+++ Src/PChecker/CheckerCore/Feedback/EventTimeLineObserver.cs
@@ -74,7 +74,7 @@ public class EventTimeLineObserver: IActorRuntimeLog
     public void OnDequeueEvent(ActorId id, string stateName, Event e)
     {
         Timeline.TryAdd(id, new());
-        if (_eventQueue.Count > 20)
+        if (_eventQueue.Count > 10)
         {
             var first = _eventQueue.First!.Value;
             if (Timeline[_eventMap[first]].First.Value.Count == 1)
