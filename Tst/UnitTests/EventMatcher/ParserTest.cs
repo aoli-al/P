961dbe26846d357fcabcc97b8cde7fc4cbb33e96
Ao Li <aolm@amazon.com>
2023-06-29 10:18:45 -0700

    update.

--- Tst/UnitTests/EventMatcher/ParserTest.cs
+++ Tst/UnitTests/EventMatcher/ParserTest.cs
@@ -4,23 +4,3 @@ using NUnit.Framework;
 using PChecker.Feedback.EventMatcher;
 
 namespace UnitTests.EventMatcher;
-
-[TestFixture]
-public class ParserTest
-{
-    [Test]
-    public void TestEventParser()
-    {
-        string input = "(E1,E2, E3)| E4";
-
-        var parser = new EventLangParser(new CommonTokenStream(new EventLangLexer(new AntlrInputStream(input))));
-        var visitor = new EventLangVisitor();
-        var node = visitor.Visit(parser.exp());
-
-        var nfa = Nfa.TreeToNFA(node);
-        nfa.Show();
-        var dfa = Dfa.SubsetConstruct(nfa);
-        dfa.Show();
-
-    }
-}
